a103ebe711e75b4d833551146ea8afa21efa50a0
Julio Francisco Motta Claudio <59737000+FranciscoMotta@users.noreply.github.com>
2022-09-14 18:04:14 -0500

    codes 1

---
+++ LED_BLINK_1/MDK-ARM/system_stm32f3xx.d
@@ -0,0 +1,10 @@
+./system_stm32f3xx.o: ..\Src\system_stm32f3xx.c \
+  C:\Users\jmott\AppData\Local\Arm\Packs\Keil\STM32F3xx_DFP\2.2.2\Drivers\CMSIS\Device\ST\STM32F3xx\Include\stm32f3xx.h \
+  C:\Users\jmott\AppData\Local\Arm\Packs\Keil\STM32F3xx_DFP\2.2.2\Drivers\CMSIS\Device\ST\STM32F3xx\Include\stm32f303xc.h \
+  C:\Users\jmott\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
+  D:\Program\ Files\ (x86)\Keil\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\jmott\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\jmott\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\jmott\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\jmott\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\jmott\AppData\Local\Arm\Packs\Keil\STM32F3xx_DFP\2.2.2\Drivers\CMSIS\Device\ST\STM32F3xx\Include\system_stm32f3xx.h
---
+++ LED_EMISOR_DE_LUZ_BLINK/MDK-ARM/RTE/_LED_EMISOR_DE_LUZ_BLINK/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'LED_EMISOR_DE_LUZ_BLINK' 
+ * Target:  'LED_EMISOR_DE_LUZ_BLINK' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
