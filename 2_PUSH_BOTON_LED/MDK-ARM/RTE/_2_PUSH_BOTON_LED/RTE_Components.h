35bfbc8b8590b709ed3bb0b3324352a6a1d7ea1c
Julio Francisco Motta Claudio <59737000+FranciscoMotta@users.noreply.github.com>
2022-09-14 18:36:01 -0500

    codes2

---
+++ 2_PUSH_BOTON_LED/MDK-ARM/RTE/_2_PUSH_BOTON_LED/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '2_PUSH_BOTON_LED' 
+ * Target:  '2_PUSH_BOTON_LED' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
