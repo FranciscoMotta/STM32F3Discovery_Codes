538c90a1a43bb31b35c086f9c79b93cceac80ba0
Julio Francisco Motta Claudio <59737000+FranciscoMotta@users.noreply.github.com>
2022-09-14 19:52:15 -0500

    codes3

---
+++ 3_PUSH_LEDS_ROUND/MDK-ARM/RTE/_3_PUSH_LEDS_ROUND/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '3_PUSH_LEDS_ROUND' 
+ * Target:  '3_PUSH_LEDS_ROUND' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
